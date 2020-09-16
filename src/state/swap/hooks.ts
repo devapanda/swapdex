1257a1c037bf5c97490dc1a2691eaf45fe4322cf
deva panda <66827166+devapanda@users.noreply.github.com>
2020-09-16 19:36:47 +0530

    update

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -90,9 +90,9 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 }
 
 const BAD_RECIPIENT_ADDRESSES: string[] = [
-  '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', // v2 factory
+  '0xa8965c323d24764cd9ec7397a2e76dfb09ec3fc9', // v2 factory
   '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a', // v2 router 01
-  '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D' // v2 router 02
+  '0x846d9c78e270b6ea68807e65e54889a047ffdd3b' // v2 router 02
 ]
 
 /**
